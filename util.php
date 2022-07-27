71d7d3ebd3447071b577df0bddc7cd8ad0b3c627
dollarstir <kpin463@gmail.com>
2022-07-27 15:14:01 +0000

    asdsad

---
+++ util.php
@@ -0,0 +1,27 @@
+<?php
+    class Util
+    {
+        public static $GO_BACK = '98';
+
+        public static $GO_TO_MAIN_MENU = '99';
+
+        public static $API_KEY = '<>Your API KEY here'; //sandbox
+
+        public static $API_USERNAME = 'sandbox'; //sandbox
+
+        public static $COMPANY_NAME = '<SENDER ID here>';
+
+        public static $COUNTRY_CODE = 'YOu country code here e.g. +254';
+    }
+?>
+
+
+
+
+
+
+
+
+
+
+
