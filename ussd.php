b51ef78aa7c4da134bafa8e31d2a9408d1be5774
dollarstir <kpin463@gmail.com>
2022-07-27 11:34:12 +0000

    adasd

--- ussd.php
+++ ussd.php
@@ -1,20 +1,32 @@
 <?php
 
+// Read the variables sent via POST from our API
 $sessionId = $_POST['sessionId'];
 $serviceCode = $_POST['serviceCode'];
 $phoneNumber = $_POST['phoneNumber'];
 $text = $_POST['text'];
 
 if ($text == '') {
-    $response = 'CON What would you like to check \n';
-    $response .= '1. My Account \n';
-    $response .= '2. Phone Number';
+    // This is the first request. Note how we start the response with CON
+    $response = "CON What would you want to check \n";
+    $response .= "1. My Account \n";
+    $response .= '2. My phone number';
 } elseif ($text == '1') {
-    $response = 'CON Choose account information you like to view \n';
-    $response .= '1. Account Number\n';
-    $response .= '2. Account Balance';
+    // Business logic for first level response
+    $response = "CON Choose account information you want to view \n";
+    $response .= "1. Account number \n";
 } elseif ($text == '2') {
-    $response = 'END Your Phone number is '.$phoneNumber;
+    // Business logic for first level response
+    // This is a terminal request. Note how we start the response with END
+    $response = 'END Your phone number is '.$phoneNumber;
+} elseif ($text == '1*1') {
+    // This is a second level response where the user selected 1 in the first instance
+    $accountNumber = 'ACC1001';
+
+    // This is a terminal request. Note how we start the response with END
+    $response = 'END Your account number is '.$accountNumber;
 }
-header('content-type:text/plain');
+
+// Echo the response back to the API
+header('Content-type: text/plain');
 echo $response;
