379041ad66f2c66c431866d23cae7a7e88015a17
dollarstir <kpin463@gmail.com>
2022-07-27 11:10:27 +0000

    sdfuhds

--- ussd.php
+++ ussd.php
@@ -6,5 +6,13 @@ $phoneNumber = $_POST['phoneNumber'];
 $text = $_POST['text'];
 
 if ($text == '') {
-    $response = 'CON What would you like to check';
+    $response = 'CON What would you like to check \n';
+    $response .= '1. My Account \n';
+    $response .= '2. Phone Number';
+} elseif ($text == '1') {
+    $response = 'CON Choose account information you like to view \n';
+    $response .= '1. Account Number\n';
+    $response .= '2. Account Balance';
+} elseif ($text == '2') {
+    $response = 'END Your Phone number is '.$phoneNumber;
 }
